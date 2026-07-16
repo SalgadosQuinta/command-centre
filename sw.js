252f25064a57807d972f2c956044d336f1aba927
Claude build <build@local>
2026-07-16 16:44:08 +0000

    Finance view: revenue pipeline (expected/invoiced/received, per client, recurring) with 30/60-day expected totals, payments to make with recurring schedules, mark-paid advancing next due, overdue flags, net position tiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* GTD Command Centre service worker */
-const CACHE = 'gtdcc-v11';
+const CACHE = 'gtdcc-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
